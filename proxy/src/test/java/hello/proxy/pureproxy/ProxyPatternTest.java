5fd81cc103ff04ae7a78b9c49b5144e59ea52e7a
Ea-Bell <cjp975@naver.com>
2022-02-27 17:04:03 +0900

    프록시 패턴과 데코레이터 패턴
    프록시, 프록시패턴, 데코레이터패턴 -소개
    ===============================================================================
    클라이언트는 서버에 필요한 것을 요청하고, 서버는 클라이언트의 요청을 처리하는 것이다.
    
    프록시의 주요 기능
    프록시를 통해서 할 수 있는 일은 크게 2가지로 구분할 수 있다.
    * 접근 제어
     * 권한에 따른 접근 차단
     * 캐싱
     * 지연 로딩
    * 부가 기능 추가
     * 원래 서버가 제공하는 기능에 더해서 부가 기능을 수행한다.
     * 예) 요청 값이나, 응답 값을 중간에 변형한다.
     * 예) 실행 시간을 측정해서 추가 로그를 남긴다.
    
     GOF 디자인 패턴
     둘다 프록시를 사용하는 방법이지만 GOF 디자인 패턴에서는 이 둘을 "의도(intent)"에 따라서 프록시
     패턴과 데코레이터 패턴으로 구분한다.
    
     (프록시 패턴, 프록시는 다른 뜻을 가지고 있다.)
     * 프록시 패턴: 접근 제어가 목적
     * 데코레이터 패턴: 새로운 기능 추가가 목적
    
     참고: 프록시라는 개념은 클라이언트 서버라는 큰 개념안에서 자연스럽게 발생할 수 있다. 프록시는 객체안에서의 개념도 있고 웹 서버에서의 프록시도 있다. 객체안에서 객체로 구현되어있는가, 웹 서버로 구현되어 잇는가 처럼 규모의 차이가 있을 뿐 근본적인 역할은 같다.
    ===============================================================================
    
    프록시 패턴 - 예제 코드1
    
    lient.execute()을 3번 호출하면 다음과 같이 처리된다.
    1. client -> realSubject 를 호출해서 값을 조회한다. (1초)
    2. client -> realSubject 를 호출해서 값을 조회한다. (1초)
    3. client -> realSubject 를 호출해서 값을 조회한다. (1초)
    
    그런데 이 데이터가 한번 조회하면 변하지 않는 데이터라면 어딘가에 보관해두고 이미 조회한 데이터를
    사용하는 것이 성능상 좋다. 이런 것을 캐시라고 한다.
    프록시 패턴의 주요 기능은 접근 제어이다. 캐시도 접근 자체를 제어하는 기능 중 하나이다.

---
+++ proxy/src/test/java/hello/proxy/pureproxy/ProxyPatternTest.java
@@ -0,0 +1,17 @@
+package hello.proxy.pureproxy;
+
+import hello.proxy.pureproxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient proxyPatternClient = new ProxyPatternClient(realSubject);
+        proxyPatternClient.execute();
+        proxyPatternClient.execute();
+        proxyPatternClient.execute();
+    }
+}
